96c97103ed57bd20d3f0507b3b770cc9cfd53066
MaedaTsuyoyo <71215094+MaedaTsuyoyo@users.noreply.github.com>
2020-09-17 19:48:08 +0900

    Update CountDate.java

--- CountDate.java
+++ CountDate.java
@@ -25,24 +25,19 @@ public class CountDate extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         request.setCharacterEncoding("UTF-8");
-        // 本日の日付を取得するためカレンダーインスタンスを取得する
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
-        //この部分を入力getParameterに変更する
         String inputString = "1970/02/09";
         String today = sdf.format(cal.getTime());
         try {
-            //Dateのインスタンスを取得する
             Date date1 = sdf.parse(inputString);
             Date date2 = sdf.parse(today);
-            //今日の日付と入力された日付の差分を取得単位はミリ秒
             long diff = date2.getTime() - date1.getTime();
 
-            //HTML出力
             Page.header(out);
             out.println("今日の日付は" + sdf.format(cal.getTime()) + "です<br/>");
-            out.println("ささきさんがいままで生きた日数は" + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) + "日です");
+            out.println("まえださんがいままで生きた日数は" + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) + "日です");
             out.println(diff / (1000 * 60 * 60 * 24 ));
             Page.footer(out);
         } catch (ParseException e) {
